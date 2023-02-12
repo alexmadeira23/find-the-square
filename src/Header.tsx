bfb9f540e56d893efae40c21c087ff657f3c4f21
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2023-02-12 18:18:43 +0000

    added animation to help hover

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,7 +5,6 @@ export function Header(props: {restart: () => void, onHelp: () => void}) {
             <div className="header-title"><h1 onClick={props.restart}>Find The Square</h1></div>
             <div className="header-info">
                 <img
-                    style={{filter: "brightness(100%) contrast(180%) grayscale(100%)", backgroundColor: "#538d4e"}}
                     src="./images/info.png" 
                     alt="help"
                     width="40px"
